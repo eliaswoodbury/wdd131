23f93444e3133cdc10b4d84c6ad3543705269e7d
eliaswoodbury <eliaswoodbury@gmail.com>
2025-01-18 23:14:39 -0700

    light/darkmode

---
+++ notes/notes.js
@@ -0,0 +1,26 @@
+// Java script can be used in front end and back end, but mostly on frontend for dynamic stuff and interactivity
+// Java and Javascript are not the same thing
+// 
+// Java Script does the following:
+// Access Content
+// Modify Content
+// Program Rules (IF, AND, OR, WHEN) LOOPS/CONDITIONS
+// 
+// Where to put Javascript internally (in html):
+//
+//@head
+<script>
+
+</script>
+// @bottom of body 
+// 
+// Where to put Javascript externally:
+// In a .js file 
+// Can be used multiple time and avoids redundancy
+// 
+// When Java is used as a .js file, the script needs to be conncected as follows:
+//<script src="filename"></script>
+// 
+// 
+// 
+// 
